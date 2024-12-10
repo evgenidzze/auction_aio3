c7fd7f0148c3dd123ebe2e41fda7c793e07c75af
evgenidzze <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>
2024-12-10 17:34:41 +0200

    add webhook MERCHANT.ONBOARDING.COMPLETED

--- whook_paypal.py
+++ whook_paypal.py
@@ -1,6 +1,12 @@
+import logging
+
+import aiohttp
+from aiohttp import BasicAuth
 from fastapi import FastAPI, Request
 
 from database.db_manage import update_user_sql
+from keyboards.admin_kb import admin_menu_kb
+from utils.config import PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET
 from utils.create_bot import bot
 
 app = FastAPI()
@@ -11,14 +17,25 @@ async def paypal_webhook(request: Request):
     payload = await request.json()
     event_type = payload.get("event_type")
     resource = payload.get("resource", {})
-    print(payload)
+    logging.info(payload)
     if event_type == "MERCHANT.ONBOARDING.COMPLETED":
         merchant_id = resource.get("merchant_id")
         email = resource.get("email")
-        user_id = resource.get("tracking_id")
+        user_id = await get_tracking_id_paypal(resource)
         await update_user_sql(telegram_id=user_id, merchant_id=merchant_id)
-        await bot.send_message(chat_id=user_id, text="Вітаю ваш PayPal під'єднано до партнерської програми бота!.")
-        print(f"Merchant {merchant_id} completed onboarding with email {email}")
+        try:
+            await bot.send_message(chat_id=user_id, text="Вітаю ваш PayPal під'єднано до партнерської програми бота!", reply_markup=admin_menu_kb.as_markup())
+        except Exception as err:
+            logging.info(err)
         return {"status": "processed"}
     return {"status": "ignored"}
 
+
+async def get_tracking_id_paypal(resource):
+    links = resource.get('links')
+    href = links[0].get('href')
+    async with aiohttp.ClientSession() as session:
+        async with session.get(href, auth=BasicAuth(PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET)) as response:
+            data = await response.json()
+            tracking_id = data.get('tracking_id')
+            return tracking_id
