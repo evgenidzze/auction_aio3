591436dba89859d9de235d08f2c91b85bff57e4b
evgenidzze <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>
2025-03-13 16:07:57 +0200

    delete webhook

--- start_bot.py
+++ start_bot.py
@@ -21,8 +21,9 @@ async def main():
     Localization(i18n=i18n).setup(router)
     bot.session.middleware(ChangeLanguageMiddleware())
     dp.startup.register(on_startup)
-    await bot(DeleteWebhook(drop_pending_updates=True))
     dp.include_router(router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
     await bot.set_my_commands([BotCommand(command='start', description='Change language/Змінити мову'),
                                BotCommand(command='main_menu', description='Main menu/Головне меню'),
                                BotCommand(command='admin', description='Group owner menu/Меню власника групи'),
