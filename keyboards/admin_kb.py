787e281ec2e79218417dc28b0ad60edcc8ea690d
evgenidzze <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>
2025-03-20 18:10:00 +0200

    redundant func rm

--- keyboards/admin_kb.py
+++ keyboards/admin_kb.py
@@ -1,20 +1,6 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-from utils.create_bot import _
 from utils.utils import payment_link_generate, GroupTypeSubscription
-
-
-async def create_subscription_group_buttons_kb(chat_id, is_trial=False):
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text='🔑 Пробний період (14 днів)',
-                              callback_data=f'subscription_group:{GroupTypeSubscription.FREE_TRIAL}:14:{chat_id}')] if is_trial else [],
-        [InlineKeyboardButton(text='🔑 Підписка на аукціон (1 місяць)',
-                              callback_data=f'subscription_group:{GroupTypeSubscription.AUCTION}:30:{chat_id}')],
-        [InlineKeyboardButton(text='🔑 Підписка на оголошення (1 місяць)',
-                              callback_data=f'subscription_group:{GroupTypeSubscription.ADVERTISEMENT}:30:{chat_id}')],
-    ])
-
 
 back_to_admin_btn = InlineKeyboardButton(text='« Назад', callback_data='admin')
 back_to_group_manage_btn = InlineKeyboardButton(text="« Назад", callback_data="group_manage")
@@ -37,21 +23,18 @@ admin_menu_kb.row(my_channels_groups_btn, add_group_kb).row(monetization, black_
 back_to_admin_kb = InlineKeyboardMarkup(inline_keyboard=[[back_to_admin_btn]])
 
 
-async def activate_ad_auction_kb(auction_token, ads_token, group_id, back_btn, free_trial):
+async def activate_ad_auction_kb(auction_token, ads_token, group_id, free_trial):
     builder = InlineKeyboardBuilder()
-
-    if auction_token or ads_token:
-        if auction_token:
-            auction_payment_url = await payment_link_generate(auction_token)
-            builder.button(text='Активувати аукціон', url=auction_payment_url)
-        if ads_token:
-            ads_payment_url = await payment_link_generate(ads_token)
-            builder.button(text='Активувати оголошення', url=ads_payment_url)
-        if free_trial == 0:
-            builder.button(text='🔑 Пробний період (14 днів)',
-                           callback_data=f'subscription_group:{GroupTypeSubscription.FREE_TRIAL}:14:{group_id}')
-        builder.button(text=_('🔄 Оновити статус'),
-                       callback_data=f'{group_id}:{auction_token},{ads_token}:sub_update')
-    builder.add(back_btn)
+    if auction_token:
+        auction_payment_url = await payment_link_generate(auction_token)
+        builder.button(text='🔑 Підписка на аукціон (1 місяць)', url=auction_payment_url)
+    if ads_token:
+        ads_payment_url = await payment_link_generate(ads_token)
+        builder.button(text='🔑 Підписка на оголошення (1 місяць)', url=ads_payment_url)
+    if free_trial == 0:
+        builder.button(text='🔑 Пробний період (14 днів)',
+                       callback_data=f'subscription_group:{GroupTypeSubscription.FREE_TRIAL}:14:{group_id}')
+    builder.add(back_my_channels_groups)
     builder.adjust(2)
     return builder.as_markup()
+
