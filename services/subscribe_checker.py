66a5737fd7ff3e53dd3496b25f0ae8002f37b432
evgenidzze <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>
2025-03-20 18:15:08 +0200

    message optimization

--- services/subscribe_checker.py
+++ services/subscribe_checker.py
@@ -2,10 +2,6 @@
 Сервіс для перевірки закінчення підписок на групи.
 """
 import logging
-
-# TODO: Можна як сервіс, а можна включити в основний файл бота або через subprocess або ще варіанти.
-#  Якщо саме так залишити то в докер контейнери включити запуск.
-
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from sqlalchemy.future import select
@@ -13,13 +9,15 @@ from datetime import datetime
 from sqlalchemy.orm import selectinload
 
 from database.services.base import engine
+from database.services.group_subscription_plan_service import GroupSubscriptionPlanService
 from utils.config import BOT_TOKEN, DEV_ID
-from database.models.channel_group import ChannelGroup
 from database.models.group_subscription_plan import GroupSubscriptionPlan
-from keyboards.admin_kb import create_subscription_group_buttons_kb
+from keyboards.admin_kb import activate_ad_auction_kb
 from aiogram import Bot
 from aiogram.utils.i18n import I18n
 from pathlib import Path
+
+from utils.utils import GroupTypeSubscription, get_tokens_and_finish_dates
 
 # Ініціалізуємо локалізацію
 _ = I18n(path=Path(__file__).parent.parent / 'locales', domain='auction').gettext
@@ -32,44 +30,44 @@ CHECK_INTERVAL_SECONDS = 300  # Інтервал перевірки (секун�
 bot = Bot(token=BOT_TOKEN, validate_token=False)
 
 
-async def send_end_subscription_message(owner_id: str, chat_id: str, group_title: str, type_subscription: str):
+async def send_end_subscription_message(owner_id: str, group_id: str, group_title: str,
+                                        type_subscription: GroupTypeSubscription):
     """
-    Відправляє повідомлення про закінчення підписки на групу.
+    Відправляє власнику групи та в групу повідомлення про закінчення підписки.
     """
     message = {
-        "auction": [
-            _("Підписка на аукціон закінчилася"),
-            _("Роботу аукціону було призупинено на невизначений час, всі дані збережено. Для поновлення роботи зверніться до адміністратора бота."),
-        ],
-        "ads": [
-            _("Підписка на оголошення закінчилася"),
-            _("Роботу оголошень було призупинено на невизначений час, всі дані збережено. Для поновлення роботи зверніться до адміністратора бота."),
-        ],
+        "auction": {
+            "owner_message": _("Підписка на аукціон закінчилася"),
+            "group_message": _(
+                "Роботу аукціону було призупинено на невизначений час, всі дані збережено. Для поновлення роботи зверніться до адміністратора бота."),
+        },
+        "ads": {
+            "owner_message": _("Підписка на оголошення закінчилася"),
+            "group_message": _(
+                "Роботу оголошень було призупинено на невизначений час, всі дані збережено. Для поновлення роботи зверніться до адміністратора бота."),
+        },
     }[type_subscription]
-
+    owner_message = message.get('owner_message')
+    group_message = message.get('group_message')
     # to owner
-    await bot.send_message(owner_id, f"{message[0]} '{group_title}'",
-                           reply_markup=await create_subscription_group_buttons_kb(chat_id))
+    chat_subscription = await GroupSubscriptionPlanService.get_subscription(group_id)
+    sub_dates, tokens = await get_tokens_and_finish_dates(group_id, chat_subscription=chat_subscription)
+    owner_kb = await activate_ad_auction_kb(auction_token=tokens[GroupTypeSubscription.AUCTION],
+                                            ads_token=tokens[GroupTypeSubscription.ADVERTISEMENT], group_id=group_id,
+                                            free_trial=chat_subscription.free_trial)
+    await bot.send_message(chat_id=owner_id, text=f"{owner_message} '{group_title}'",
+                           reply_markup=owner_kb)
     # to groups
-    await bot.send_message(chat_id, message[1])
+    await bot.send_message(group_id, group_message)
 
 
 async def process_expired_flags(session: AsyncSession):
     """
     Проходить по групах, у яких закінчився час `auction_time` або `ads_time`,
-    оновлює відповідні флажки та повертає список груп для подальшого використання.
+    оновлює відповідні флажки та повертає список груп.
     """
     current_timestamp = datetime.utcnow().timestamp()
 
-    # Вибираємо групи, у яких закінчився auction_time або ads_time
-    # query = (
-    #     select(ChannelGroup)
-    #     .where(
-    #         (ChannelGroup.subscription_plan.auction_sub_time < current_timestamp) & ChannelGroup.subscription_plan.auction_paid.is_(True) |
-    #         (ChannelGroup.subscription_plan.ads_sub_time < current_timestamp) & ChannelGroup.subscription_plan.ads_paid.is_(True)
-    #     )
-    #     .limit(BATCH_SIZE)
-    # )
     query = (
         select(GroupSubscriptionPlan)
         .options(selectinload(GroupSubscriptionPlan.group))
@@ -96,11 +94,13 @@ async def process_expired_flags(session: AsyncSession):
         group = subscription.group
         groups.append(group)
         if subscription.auction_sub_time < current_timestamp and subscription.auction_paid:
-            subscription.auction_paid = False
-            await send_end_subscription_message(group.owner_telegram_id, group.chat_id, group.chat_name, "auction")
+            subscription.auction_paid = False  # функція стає безкоштовною
+            await send_end_subscription_message(group.owner_telegram_id, group.chat_id, group.chat_name,
+                                                GroupTypeSubscription.AUCTION)
         if subscription.ads_sub_time < current_timestamp and subscription.ads_paid:
-            subscription.ads_paid = False
-            await send_end_subscription_message(group.owner_telegram_id, group.chat_id, group.chat_name, "ads")
+            subscription.ads_paid = False  # функція стає безкоштовною
+            await send_end_subscription_message(group.owner_telegram_id, group.chat_id, group.chat_name,
+                                                GroupTypeSubscription.ADVERTISEMENT)
 
     # Зберігаємо зміни
     await session.commit()
