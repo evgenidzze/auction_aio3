2fc967771b3fd2f5ee711374b82203591941fc7a
evgenidzze <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>
2025-03-21 19:08:58 +0200

    core types

---
+++ utils/core_types.py
@@ -0,0 +1,46 @@
+from enum import Enum
+
+
+class UserTypeSubscription(str, Enum):
+    """
+    This object represents a type of User subscription
+    """
+
+    ADVERTISEMENT = 'ads'
+    AUCTION = 'auction'
+
+
+class GroupTypeSubscription(str, Enum):
+    """
+    This object represents a type of Group subscription
+    """
+
+    ADVERTISEMENT = 'ads'
+    AUCTION = 'auction'
+    FREE_TRIAL = 'free_trial'
+
+    @staticmethod
+    async def get_by_product_category(category):
+        data = {
+            ClientProductCategory.AUCTION: GroupTypeSubscription.AUCTION,
+            ClientProductCategory.ADVERTISEMENT: GroupTypeSubscription.ADVERTISEMENT,
+            AdminProductCategory.AUCTION: GroupTypeSubscription.AUCTION,
+            AdminProductCategory.ADVERTISEMENT: GroupTypeSubscription.ADVERTISEMENT,
+        }
+        return data.get(category)
+
+
+class ClientProductCategory(str, Enum):
+    AUCTION = 'CLIENT_AUCTION'
+    ADVERTISEMENT = 'CLIENT_ADVERTISEMENT'
+
+
+class AdminProductCategory(str, Enum):
+    AUCTION = 'ADMIN_AUCTION'
+    ADVERTISEMENT = 'ADMIN_ADVERTISEMENT'
+
+    @staticmethod
+    async def get_by_func_type(func_type):
+        data = {GroupTypeSubscription.AUCTION: AdminProductCategory.AUCTION,
+                GroupTypeSubscription.ADVERTISEMENT: AdminProductCategory.ADVERTISEMENT}
+        return data.get(func_type)
